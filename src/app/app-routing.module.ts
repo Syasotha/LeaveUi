a0b62d330bd19bd9812b28c8031767fa3767b75e
kajaluxshi <luxshibala@gmail.com>
2018-10-23 13:42:11 +0530

    LeaveType Integration Finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RoleComponent } from './Components/role/role.component';
 import { DepartmentComponent } from './Components/department/department.component';
+import { LeavetypeComponent } from './Components/leavetype/leavetype.component';
 
 const routes: Routes = [
   {path: 'rolename', component:RoleComponent},
   {path: 'department', component:DepartmentComponent},
+  {path:'leavetype',component:LeavetypeComponent},
 ];
 
 @NgModule({
