6b8fade9625374f7cf4ba9a51fc1e58dd2ba0146
Yasotha <yasoselva1992@gmail.com>
2018-10-23 14:52:28 +0530

    department curd

--- src/app/Components/department/department.component.ts
+++ src/app/Components/department/department.component.ts
@@ -12,4 +12,25 @@ export class DepartmentComponent implements OnInit {
   ngOnInit() {
   }
 
+  getDetailsId(departmentDetailsId){
+    this.departmentDetailsobj=Object.assign({},departmentDetailsId)
+
+  }
+
+  deleteDepartment(DepartmentDeletDetails){
+    this.departmentService.deleteDepartment(DepartmentDeletDetails).subscribe(departmentDelete=>{
+      this.departmentDetailsobj.id=DepartmentDeletDetails.id;
+      this.getDepartment();
+      alert("Delete Department Sucessfully");
+    });
+
+  }
+
+  UpdateDepartmentDetails(){
+    this.departmentService.editDepartmentById(this.departmentDetailsobj).subscribe(updatedDepartment=>{
+    this.getDepartment();
+    alert("Updated Department Sucessfully");
+    });
+  }
+
 }
--- src/app/Services/department.service.ts
+++ src/app/Services/department.service.ts
@@ -5,5 +5,26 @@ import { Injectable } from '@angular/core';
 })
 export class DepartmentService {
 
+<<<<<<< Updated upstream
   constructor() { }
+=======
+  constructor(private httpObj: HttpClient) { }
+private departmentUrl='http://localhost:8080/department';
+
+getAllDepartment(){
+  return this.httpObj.get<Department[]>(this.departmentUrl);
+
+}
+createDepartment(data){
+  return this.httpObj.post<Department>(this.departmentUrl,data);
+}
+
+deleteDepartment(DepartmentDelet){
+  return this.httpObj.delete(this.departmentUrl+ "/" +DepartmentDelet.id);
+}
+
+editDepartmentById(editDepartmentDetails){
+  return this.httpObj.put<Department>(this.departmentUrl,editDepartmentDetails);
+}
+>>>>>>> Stashed changes
 }
