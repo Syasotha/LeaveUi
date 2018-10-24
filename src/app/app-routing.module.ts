bacb161b510418fdc9200f7356c2c33228be86b7
Yasotha <yasoselva1992@gmail.com>
2018-10-24 14:30:54 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { RoleComponent } from './Components/role/role.component';
 import { DepartmentComponent } from './Components/department/department.component';
 import { StatusComponent } from './Components/status/status.component';
 import { UserComponent } from './user/user.component';
+import { LeavetypeComponent } from './Components/leavetype/leavetype.component';
 
 const routes: Routes = [
   {path: 'rolename', component:RoleComponent},
   {path: 'department', component:DepartmentComponent},
   {path: 'status', component:StatusComponent},
   {path: 'user', component:UserComponent},
+  {path: 'leaveType', component:LeavetypeComponent},
 ];
 
 @NgModule({
